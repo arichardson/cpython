47fd31e3bcc5f95fa6ded806fa8f00600db9a276
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-03-02 13:25:24 +0000

    ./configure passes.

---
+++ build-for-cheribsd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+CHERI_ROOT="${HOME}/cheri"
+CHERISDK="${CHERI_ROOT}/output/sdk256/bin"
+CHERIBSD_SYSROOT="${CHERI_ROOT}/output/sdk256/sysroot"
+INSTALL_DIR=${CHERI_ROOT}/output/rootfs256
+
+export PATH=${CHERISDK}:${CHERILDDIR}:$PATH
+export CC=${CHERISDK}/clang
+export CXX=${CHERISDK}/clang++
+COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -msoft-float -O0 -static -integrated-as"
+COMPILE_FLAGS="${COMMON_FLAGS} -Werror=cheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal -Werror=incompatible-pointer-types"
+
+./configure MACHDEP=freebsd "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} -pthread -Wl,-melf64btsmip_cheri_fbsd" --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --disable-shared  --disable-ipv6
+gmake -j8
+#gmake install DESTDIR=${INSTALL_DIR}
+
